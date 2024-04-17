d0a82f52214b84d344d67b9a0f30660db0463ebc
Atij Mahesh <atijmahesh914@gmail.com>
2024-04-16 17:14:46 -0700

    Update pipe.c

--- lab1/pipe.c
+++ lab1/pipe.c
@@ -6,74 +6,73 @@
 
 int main(int argc, char *argv[])
 {
-	//first arg is pipe command anyway
-	if(argc <= 1) {
-		printf("parse error near '|'\n");
-		return 1;
-	}
-	
-	pid_t ids[argc-1];
-	int pipes[argc-2][2];
-	int status;
+    if (argc <= 1) {
+        printf("Must have at least one shell command.\n");
+        return 1;
+    }
+	pid_t process_ids[argc-1];
+    int pipe_fds[argc-2][2];
 
-	int in_fd = 0;
+    //Create pipes
+    for (int i = 0; i < argc-2; i++) {
+        if (pipe(pipe_fds[i]) == -1) {
+            perror("pipe");
+            exit(1);
+        }
+    }
 
-	//main loop
-	for(int i = 1; i<argc; i++) {
-		if(i< argc-1) {
-			if(pipe(pipes[i]) == -1) {
-				perror("pipe");
-				exit(1);
-			}	
-		}
-
-		ids[i-1] = fork();
-		//failed fork
-		if(ids[i]==-1) {
-			perror("fork");
-			exit(1);
-		}
-
-		if(ids[i-1] == 0) { //child
-			if(in_fd != 0) {
-				dup2(in_fd, STDIN_FILENO);
-				close(in_fd);
-			}
-
-			if (i < argc - 1) {
-                close(pipes[i-1][0]);
-                dup2(pipes[i-1][1], STDOUT_FILENO);
-                close(pipes[i-1][1]);
+    for (int i = 0; i < argc-1; i++) {
+        //Fork a child process for each command
+        process_ids[i] = fork();
+        
+        //Fork failure
+        if (process_ids[i] == -1) {
+            perror("fork");
+            exit(1);
+        } 
+        
+        if (process_ids[i] == 0) {
+            // Child process
+            if (i >= 1) {
+                // Redirect stdin from the read end of the last pipe
+                dup2(pipe_fds[i-1][0], STDIN_FILENO);
+                close(pipe_fds[i-1][0]);
             }
 
-            execlp(argv[i], argv[i], NULL);
+            if (i < argc-2) {
+                //Redirect stdout to the write end of the current pipe
+                dup2(pipe_fds[i][1], STDOUT_FILENO);
+                close(pipe_fds[i][1]);
+            }
+
+            //Close all pipe file descriptors
+            for (int j = 0; j < argc-2; j++) {
+                close(pipe_fds[j][0]);
+                close(pipe_fds[j][1]);
+            }
+
+            //Execute cmd
+            execlp(argv[i+1], argv[i+1], NULL);
             perror("execlp");
             exit(1);
-		}
-		else { //parent
-			if (in_fd != 0) {
-                close(in_fd);
-            }
-            if (i < argc - 1) {
-                close(pipes[i-1][1]);
-                in_fd = pipes[i-1][0]; 
-            }
-		}
-	}
-	
-	//close last used input
-	if(in_fd!=0) close(in_fd);
+        }
+    }
 
+    //Close pipe file descriptors in the parent
+    for (int i = 0; i < argc-2; i++) {
+        close(pipe_fds[i][0]);
+        close(pipe_fds[i][1]);
+    }
 
-	//wait for all child proc to finish
-	for(int i = 0; i<argc-1; i++) {
-		waitpid(ids[i], &status, 0);
-		if(!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
-			printf("Command '%s' failed with exit code %d\n", 
-			argv[i+1], WEXITSTATUS(status));
-			return 1;
-		}
-	}
+    //Wait child processes to finish
+    for (int i = 0; i < argc-1; i++) {
+        int status;
+        waitpid(process_ids[i], &status, 0);
+        if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
+            printf("Command '%s' failed with exit code %d\n", argv[i+1], WEXITSTATUS(status));
+            return 1;
+        }
+    }
 
-	return 0;
+    return 0;
 }
